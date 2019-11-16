f6a0e088a4d545828cd882bec7cb22b97620bde4
ripee <ripee.dev@gmail.com>
2019-11-16 02:44:03 +0100

    noblelte-common: Add back updated Vendor security patch level

--- noblelte-common.mk
+++ noblelte-common.mk
@@ -350,6 +350,10 @@ PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-impl \
     android.hardware.usb@1.0-service.basic
 
+# Vendor security patch level
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.lineage.build.vendor_security_patch=2018-08-01
+
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-service.zero
