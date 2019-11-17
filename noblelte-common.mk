19a9808bb28087c9fa538f8d05f39f027fb17659
ripee <ripee.dev@gmail.com>
2019-11-17 22:04:53 +0100

    noblelte-common: Make the external camera provider ignore internal cameras
    * The external camera provider occupies our camera v4l2 nodes,
      potentially crashing the kernel driver and blocking the camera HAL.
      Unfortunately, there's no easy way to just disable it, so let's just
      tell it to ignore the internal video devices.
    
    Change-Id: I00c67de1b94a1f7eca1bf458d1d0e4de3507aa81
    Signed-off-by: Daniel Jarai <jaraidaniel@gmail.com>

--- noblelte-common.mk
+++ noblelte-common.mk
@@ -112,6 +112,9 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl.zero \
     android.hardware.camera.provider@2.4-service
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/external_camera_config.xml:system/vendor/etc/external_camera_config.xml
+
 # Configstore
 PRODUCT_PACKAGES += \
     android.hardware.configstore@1.0-impl \
