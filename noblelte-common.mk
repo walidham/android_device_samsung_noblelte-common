da7217f0e2785d2167b13d98810445e846eca7f4
enesuzun2002 <enesuzun200227@gmail.com>
2019-12-05 22:16:23 +0100

    noblelte-common: Move mixer_paths_0.xml back to common tree

--- noblelte-common.mk
+++ noblelte-common.mk
@@ -88,7 +88,8 @@ PRODUCT_COPY_FILES += \
 # Audio
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio_effects.conf:system/vendor/etc/audio_effects.conf \
-    $(LOCAL_PATH)/configs/audio_policy.conf:system/vendor/etc/audio_policy.conf
+    $(LOCAL_PATH)/configs/audio_policy.conf:system/vendor/etc/audio_policy.conf \
+    $(LOCAL_PATH)/configs/mixer_paths_0.xml:system/vendor/etc/mixer_paths_0.xml
 
 PRODUCT_PACKAGES += \
     audio.primary.universal7420 \
