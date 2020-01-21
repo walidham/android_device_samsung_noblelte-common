f9ae83d02a032b9a35717efcc4c342b41c874f82
enesuzun2002 <enesuzun200227@gmail.com>
2020-01-21 05:33:27 +0000

    zero: drop custom BT hidl-impl

--- noblelte-common.mk
+++ noblelte-common.mk
@@ -92,7 +92,7 @@ PRODUCT_COPY_FILES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-	android.hardware.bluetooth@1.0-impl.zero \
+	android.hardware.bluetooth@1.0-impl \
 	android.hardware.bluetooth@1.0-service \
 	libbt-vendor
 
